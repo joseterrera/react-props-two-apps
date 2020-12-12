3f7c35881a9cd588f28090e142a8910394a788d3
Jose Terrera <joseterrera@yahoo.com>
2020-12-11 17:33:42 -0800

    adding component blackjackgame

--- blackjack-app/src/BlackjackGame.js
+++ blackjack-app/src/BlackjackGame.js
@@ -18,3 +18,31 @@ function getCard(values, suits) {
   return randomVal + randomSuit
 
 }
+
+
+function getCards(values, suits, numCards = 1) {
+  const cardsArr = [getCard(values, suits)];
+  while (cardsArr.length < numCards) {
+    let randomCard = getCard(values, suits);
+    if( !cardsArr.includes(randomCard)) cardsArr.push(randomCard);
+  }
+  return cardsArr;
+}
+
+/* main game component */
+function BlackjackGame(props) {
+  const pairOfCards = getCards(props.values, props.suits, 2);
+  return (
+    <div className="BlackjackGame">
+      <CardList cards={pairOfCards} />
+      <Score cards= {pairOfCards} />
+    </div>
+  );
+}
+
+BlackjackGame.defaultProps = {
+  values: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "0", "J", "Q", "K"],
+  suits: ["C", "D", "H", "S"]
+};
+
+export default BlackjackGame;
